e5fb4afa68f2b4f0b785bde433e3ef5da0e6ab51
tatilarsen <tatilarsen@gmail.com>
2024-10-26 10:40:32 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const greeting = document.getElementById("greeting");
+greeting.style.textAlign = "center"
+greeting.style.marginTop = "50px"
+greeting.style.fontSize = "50px"
+const username = window.prompt("What's your name?", "Jane");
+greeting.innerHTML = "Welcome " + username + "!";
